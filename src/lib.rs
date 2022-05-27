6a6646fa2b5193adc080083b4e8292c6715ae2cb
Gaurav Tiwari <gaurav1@tutamail.com>
2022-05-27 14:35:48 +0530

    Revert "feat(h2c): copy hash2curve from zkcrypto/bls12_381"
    This reverts commit 69f06d02a6989c05a3f82cd31cafdd91a8faf77a.

--- src/lib.rs
+++ src/lib.rs
@@ -46,13 +46,6 @@ use ff::Field;
 use group::prime::PrimeCurveAffine;
 use pairing_lib::{Engine, MultiMillerLoop, PairingCurveAffine};
 
-/// Use the generic_array re-exported by digest to avoid a version mismatch
-#[cfg(feature = "hash_to_curve")]
-pub(crate) use digest::generic_array;
-
-#[cfg(feature = "hash_to_curve")]
-pub mod hash_to_curve;
-
 /// Bls12-381 engine
 #[derive(Debug, Copy, Clone)]
 pub struct Bls12;
